4d9f6aaac73d59a06ef182f13490f68c7bad95a1
Chloe Stamper <chloe@Chloes-MacBook-Pro.local>
2020-05-07 22:03:29 -0700

    Debugging

--- server.py
+++ server.py
@@ -353,13 +353,13 @@ def pillow_analyze_image(img_url):
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    # app.debug = True
+    app.debug = True
     # make sure templates, etc. are not cached in debug mode
     # app.jinja_env.auto_reload = app.debug
 
     connect_to_db(app)
 
     # Use the DebugToolbar
-    # DebugToolbarExtension(app)
+    DebugToolbarExtension(app)
 
     app.run(port=5000, host='0.0.0.0')
